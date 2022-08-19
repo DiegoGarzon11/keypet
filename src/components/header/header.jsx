f1f9310ea0f75384dc743a9f9b95d1b7e2a2b9a1
Diego Garzon <diegogarzon357@gmail.com>
2022-08-18 19:08:14 -0500

    cls

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,6 @@ function Header() {
 	return (
 		<>
 			<header className='header' id='header'>
-		
 				<div className='container'>
 					<img src={img} className='logo' alt='' width='250' height='210' />
 					<h1 className='title'> key-Pet</h1>
